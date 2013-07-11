3baf659bac976e2fd1607199f78e8e59e3fa7086
julieveal <julieclucas@att.net>
2013-07-10 21:48:17 -0400

    Socks in the Dark:The problem is I  can’t see in the dark.

--- Problem Solving/2.js
+++ Problem Solving/2.js
@@ -2,3 +2,7 @@ Cat, Parrot and Bag of Seed:
 Problem is we don’t know where the cabbage and the goat are. 
 We need to find out where the goat and cabbage are in order to advise a solution for getting the parrot, cat and seed to the other side of the riverbank.
 Cat can eat the goat and the goat will eat the cabbage but we don’t know if we can make multiple trips or not.
+He could make multiple trips but need to know which one should go first.
+We could take the goat first and then nothing would be eaten and go back and make 2 more trips.
+Socks in the Dark:
+The problem is I  can’t see in the dark.
