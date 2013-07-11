2807854a045d73f53e4afa086d010fd79511bdd9
julieveal <julieclucas@att.net>
2013-07-10 21:44:46 -0400

    I added initial setup.

--- Problem Solving/2.js
+++ Problem Solving/2.js
@@ -1,2 +1,3 @@
 Cat, Parrot and Bag of Seed:
 Problem is we don’t know where the cabbage and the goat are. 
+We need to find out where the goat and cabbage are in order to advise a solution for getting the parrot, cat and seed to the other side of the riverbank.
