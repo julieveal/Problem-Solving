a927f018f04db5beb42a60e58a9e4bbcbdf9cc96
julieveal <julieclucas@att.net>
2013-07-16 00:10:01 -0400

    division

--- Expressions/script.js
+++ Expressions/script.js
@@ -1,8 +1,6 @@
-//Expressions - Arithmetic Operators
+//Expressions - Modulo Operator
 
-//area of the triangle is half of the width times the height
-var width = 8;
-var height = 7;
-var area = width * height/2;
-console.log(area);
+
+var remainder = 32 % 10;
+console.log(remainder);
 
