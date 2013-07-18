0e4589bde7c1209db9d4629783b31a5444a0bdad
julieveal <julieclucas@att.net>
2013-07-17 22:53:37 -0400

    I added slices = 8 and then multiplied it in the result

--- Git Repo/veal_julie_wpf/Expressions_Worksheet/js/script.js
+++ Git Repo/veal_julie_wpf/Expressions_Worksheet/js/script.js
@@ -1,14 +1,15 @@
 //Julie Veal 7_17_13 Expression Worksheet
 
-//Sparky's real age time dog years is his dog years age.
+//Slice of pie part one. Calculated how many slices each person gets
 
 
-var age = prompt("Please enter the real age of Sparky");
-var years = prompt("What is 1 year equal to in dog years");
-var product = age * years;//calculates with that info
+var people = prompt("Please enter how many people are at your party");
+var slices = "8"
+var pizzas = prompt("How many pizzas did you order");
+var quotient = pizzas / people * slices;//calculates with that info
 
 
 //console.log(product);//prints it out to console
 
-var result = "The age of Sparky in dog years is " + product + " years ";
+var result = "Each person will get " + quotient + " slices ";
 alert(result);
