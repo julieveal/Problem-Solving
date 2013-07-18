7d082169d082c9c85e3c5c8dd4fd1e79f9c6ec1d
julieveal <julieclucas@att.net>
2013-07-17 21:55:16 -0400

    I can't firgure out how to get it to calulate the age

---
+++ Git Repo/veal_julie_wpf/Expressions_Worksheet/js/script.js
@@ -0,0 +1,13 @@
+//Julie Veal 7_17_13 Expression Worksheet
+
+//Sparky's real age time dog years is his dog years age.
+
+
+var age = 5;
+var dog years = 7;
+var equals = age * dog years;//calculates with that info
+
+
+console.log(equals);//prints it out to console
+
+//var result = "The age of Sparky in dog years is " 
