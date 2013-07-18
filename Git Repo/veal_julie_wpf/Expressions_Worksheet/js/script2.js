e5297b36a35d67754f2ebc8bb1504fa399cba73f
julieveal <julieclucas@att.net>
2013-07-17 22:34:52 -0400

    I named the pizza part one script2 so I could find it again if I need to

---
+++ Git Repo/veal_julie_wpf/Expressions_Worksheet/js/script2.js
@@ -0,0 +1,14 @@
+//Julie Veal 7_17_13 Expression Worksheet
+
+//Slice of pie part one. Calculated how many slices each person gets
+
+
+var people = prompt("Please enter how many people are at your party");
+var pizzas = prompt("How many pizzas did you order");
+var quotient = pizzas / people;//calculates with that info
+
+
+//console.log(product);//prints it out to console
+
+var result = "Each person will get " + quotient + " slices ";
+alert(result);
