1e5c5d0ab83ccfb99d6c556dc6ef5cd9685c12fe
julieveal <julieclucas@att.net>
2013-07-18 23:46:40 -0400

    It was tough adding the string but I worked it out!

---
+++ Git Repo/veal_julie_wpf/Expressions_Industry/js/script.js
@@ -0,0 +1,23 @@
+//Julie Veal 7_18_13 Industry
+
+//how man
+
+
+var a = "BIG";
+var b = "BOX";
+var boxName = a + " " + b;
+var xpositions= prompt ("How many positions are there along the x axis?"); //this is the price of the purse
+var ypositions= prompt ("How many positions are there along the y axis?");
+var total = xpositions * ypositions;
+
+
+
+//onsole.log(total);//prints it out to console
+
+
+
+
+
+var result = "There are " + xpositions + " partitions along the width of the box and " + ypositions + " along the length of the box, so I can package " + total + " bottles in my " + boxName + "!";
+alert(result);
+
