c348e2c4100735f5cc83f95d249550ae06a80b49
julieveal <julieclucas@att.net>
2013-07-18 23:51:02 -0400

    added more comments

--- Git Repo/veal_julie_wpf/Expressions_Industry/js/script.js
+++ Git Repo/veal_julie_wpf/Expressions_Industry/js/script.js
@@ -1,14 +1,14 @@
 //Julie Veal 7_18_13 Industry
 
-//how man
+//How many bottles can i fit in my box?
 
 
-var a = "BIG";
-var b = "BOX";
-var boxName = a + " " + b;
-var xpositions= prompt ("How many positions are there along the x axis?"); //this is the price of the purse
-var ypositions= prompt ("How many positions are there along the y axis?");
-var total = xpositions * ypositions;
+var a = "BIG"; //first name of my box
+var b = "BOX"; //last name of my box
+var boxName = a + " " + b; //my box's full name
+var xpositions= prompt ("How many positions are there along the x axis?"); //this is to input the number of partitions i have in the x direction of my box
+var ypositions= prompt ("How many positions are there along the y axis?"); //this is to input the number of partitions i have in the y direction of my box
+var total = xpositions * ypositions; //this line calculates the total number of partitions i have in my box, thus the total number of bottles i can package
 
 
 
@@ -20,4 +20,4 @@ var total = xpositions * ypositions;
 
 var result = "There are " + xpositions + " partitions along the width of the box and " + ypositions + " along the length of the box, so I can package " + total + " bottles in my " + boxName + "!";
 alert(result);
-
+//tuh dah!
