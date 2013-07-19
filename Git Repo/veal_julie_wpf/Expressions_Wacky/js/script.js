d9e2d0bb6f0b610313323ea3e0b47cad263c3d45
julieveal <julieclucas@att.net>
2013-07-18 21:24:14 -0400

    this error said unexpected token

---
+++ Git Repo/veal_julie_wpf/Expressions_Wacky/js/script.js
@@ -0,0 +1,16 @@
+//Julie Veal 7_18_13 wacky arrays
+
+//What are the names of my children?
+
+
+
+
+
+
+var result = "I can buy " + total + " pairs of shoes in " + months + ;
+alert(result);
+
+var myKidsNames = ["Adam", "Lucas", "Thomas"]; //0,1,2
+
+
+console.log(myKidsNames[num]);//lets me see what is in the list
