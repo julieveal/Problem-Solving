05eff29e70649e4f2320b39007a9a440ec7ee115
julieveal <julieclucas@att.net>
2013-07-18 23:52:11 -0400

    not very wacky

--- Git Repo/veal_julie_wpf/Expressions_Wacky/js/script.js
+++ Git Repo/veal_julie_wpf/Expressions_Wacky/js/script.js
@@ -4,22 +4,21 @@
 
 var son1 = prompt("I have three sons. Adam is 18."); //This is the price of the shoes
 
-var myKidsAges = ["18", "16", "14"]; //0,1,2
+var myKidsAges = [18, 16, 14]; //0,1,2
 
 var son2 = prompt("Son number 2 is 2 years younger than the oldest.");//asking to average the 3 ages, mult and divide by 3
 
 var son3 = prompt("Son number 3 is 2 years younger than son number 2.");//asking to average the 3 ages, mult and divide by 3
 
-var array_sum = myKidsAges;//asking to average the 3 ages, mult and divide by 3
-
-var average = array_sum / 3;
+var average = (myKidsAges(0) + myKidsAges(1) + myKidsAges(2)) / 3;//asking to average the 3 ages, mult and divide by 3
 
 
 
 
 
 
-var result = "Since son number 1 is " + (myKidsAges[0]) + " then son number 2 is " + (myKidsAges[1]) + " and son number 3 is " + (myKidsAges[2]) + " so the average age is " + average + ".";
+
+var result = "Since son number 1 is " + myKidsAges [0] + " then son number 2 is " + myKidsAges[1] + " and son number 3 is " + myKidsAges[2] + " so the average age is " + average + ".";
 alert(result);
 
 
