18449c4e1e20826cc6f386d1db32129cecc852ff
julieveal <julieclucas@att.net>
2013-07-18 21:40:25 -0400

    I typed in the wrong name at the bottom result

--- Git Repo/veal_julie_wpf/Expressions_Wacky/js/script.js
+++ Git Repo/veal_julie_wpf/Expressions_Wacky/js/script.js
@@ -1,16 +1,29 @@
 //Julie Veal 7_18_13 wacky arrays
 
-//What are the names of my children?
+//How many pairs of shoes can I buy in a year when my budget is $75 per month? Most shoes cost $50.
+
+
+var budget = prompt("I have three sons. How old is the oldest?"); //This is the price of the shoes
+
+var myKidsAges = ["18", "16", "14"]; //0,1,2
+
+
+
+///var months = prompt("How old is kid number one?");// asking how many months so I could multiply the budget and the months
+
+//var cost = prompt("What is the average age of the kids?");//asking for the price for shoes so I can divide the budget and months
+
+//var total = budget * months / cost //budget times the months divided by the average cost of shoes
 
 
 
 
 
 
-var result = "I can buy " + total + " pairs of shoes in " + months + ;
-alert(result);
-
-var myKidsNames = ["Adam", "Lucas", "Thomas"]; //0,1,2
+//var result = "I can buy " + total + " pairs of shoes in " + months + ;
+//alert(result);
 
 
-console.log(myKidsNames[num]);//lets me see what is in the list
+
+
+console.log(myKidsAges[0]);//lets me see how old each of them is
