5d0d101df0a49c872a576c72977611041f9ff7ab
julieveal <julieclucas@att.net>
2013-07-18 21:02:30 -0400

    added some comments

--- Git Repo/veal_julie_wpf/Expressions_Personal/js/script.js
+++ Git Repo/veal_julie_wpf/Expressions_Personal/js/script.js
@@ -5,11 +5,11 @@
 
 var budget = prompt("What is my budget for shoes per month"); //This is the price of the shoes
 
-var months = prompt("How many months in a year?");
+var months = prompt("How many months in a year?");// asking how many months so I could multiply the budget and the months
 
-var cost = prompt("What is the average price for shoes?");
+var cost = prompt("What is the average price for shoes?");//asking for the price for shoes so I can divide the budget and months
 
-var total = budget * months / cost
+var total = budget * months / cost //budget times the months divided by the average cost of shoes
 
 //console.log(total);//prints it out to console
 
