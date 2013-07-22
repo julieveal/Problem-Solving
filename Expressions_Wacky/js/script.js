edef8248922bdf7f6118fac52bbde34c40a6f4b6
julieveal <julieclucas@att.net>
2013-07-22 19:43:41 -0400

    making sure this is committed

---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,27 @@
+//Julie Veal 7_18_13 wacky arrays
+
+//Calculate the average age of the boys
+
+var son1 = prompt("I have three sons. Adam is 18."); //This is the price of the shoes
+
+var myKidsAges = [18, 16, 14]; //0,1,2
+
+var son2 = prompt("Son number 2 is 2 years younger than the oldest.");//asking to average the 3 ages, mult and divide by 3
+
+var son3 = prompt("Son number 3 is 2 years younger than son number 2.");//asking to average the 3 ages, mult and divide by 3
+
+var average = (myKidsAges(0) + myKidsAges(1) + myKidsAges(2)) / 3;//asking to average the 3 ages, mult and divide by 3
+
+
+
+
+
+
+
+var result = "Since son number 1 is " + myKidsAges [0] + " then son number 2 is " + myKidsAges[1] + " and son number 3 is " + myKidsAges[2] + " so the average age is " + average + ".";
+alert(result);
+
+
+
+
+//console.log(myKidsAges);//lets me see how old each of them is
