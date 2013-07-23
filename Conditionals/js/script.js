bad93f108a701f831d1b936562bd18cb8b53b32a
julieveal <julieclucas@att.net>
2013-07-22 21:19:33 -0400

    2 conditional expressions. one on the left and one on the right

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -4,7 +4,7 @@ var budget = 300;
 var iPhonePrice = 199.99;
 var paycheck = 200;
 //if the if the iphone price is less our budget
-if(iPhonePrice < budget && paycheck > 300){
+if(iPhonePrice < budget && paycheck > 300){      //one conditional expression on left and one on right
 	//code performed if the condition is true. 
 	console.log("We can buy the phone!");
 
