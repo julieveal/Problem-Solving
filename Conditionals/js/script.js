0e4cb6207dcf3339679e88ad05cac474010bd6d7
julieveal <julieclucas@att.net>
2013-07-22 21:16:56 -0400

    combines using &&

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -4,15 +4,11 @@ var budget = 300;
 var iPhonePrice = 199.99;
 var paycheck = 200;
 //if the if the iphone price is less our budget
-if(iPhonePrice < budget){
+if(iPhonePrice < budget && paycheck > 300){
 	//code performed if the condition is true. 
 	console.log("We can buy the phone!");
 
 }else{
 	console.log("no phone");	
 }
-if(paycheck > 300){
-	console.log("we can buy");
-}else{
-	console.log("no phone");
-}	
+	
