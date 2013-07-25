20deb5fbdb947bb6ea9031634ba7945c5a060d6e
julieveal <julieclucas@att.net>
2013-07-25 19:18:39 -0400

    it worked! Putting comments on each line helps.

--- Conditionals_Projects/Conditioonals_Personal/js/script.js
+++ Conditionals_Projects/Conditioonals_Personal/js/script.js
@@ -1,20 +1,20 @@
-// Veal_Julie  July 24, 2013  Tires
+// Veal_Julie  July 25, 2013  Personal
 
-//psi is ok between 30 and 35
-//do they pass spec
-
-
-var frontLeft = 34;
-var frontRight = 32;
+//What size tire do I put on my bike?
+//I have 4 to choose from. The wheel needs to be the same height as the tire. 
 
 
-//if the tire pressure in between 30 and 35
-if(frontLeft < 36 && frontRight > 29){
+var tireA = 10;			// 10 inches high
+	tireB = 15;			//15 inches high
+	tireC = 20;			//20 inches high
+	tireD = 25;			//25 inches high
+	bikeHeight = 20;					//how tall is the tire?
 
-	console.log("Tires pass spec!");
-
-}else{
-
-	console.log("You should tires checked!");
-
-}	
+if(tireC == bikeHeight){ //they are the same number
+							
+	console.log("Put the tire on the bike!");//the right size
+}else{									//otherwise
+										
+	console.log("You should use a different tire!");		//not the right size
+													//
+}													//
