c34cbf4d064cf4f54ead02a31ea2a6bc5bb24edd
julieveal <julieclucas@att.net>
2013-07-25 01:03:37 -0400

    added else if

---
+++ Conditionals_Worksheet/Tires/js/script.js
@@ -0,0 +1,19 @@
+// Veal_Julie  July 24, 2013  Tires
+
+//grades are ok between 30 and 35
+//do they pass spec
+
+var tires = Number(prompt("Enter tire pressure", 35));
+
+
+if(tires >= 30 && tires <= 35){
+
+	console.log("Tires pass spec!");
+
+}/*else if(tires >= 35 && tires <= 30){
+//console.log("What come after")
+	console.log("You should tires checked!");
+
+
+
+}	
