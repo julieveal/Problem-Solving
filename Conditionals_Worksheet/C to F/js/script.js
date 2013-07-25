b3e909eb0881fd8b87235b0a48dda1ad1c6644bb
julieveal <julieclucas@att.net>
2013-07-24 22:47:22 -0400

    starting over because I realized it doesn't go to C, only to F

---
+++ Conditionals_Worksheet/C to F/js/script.js
@@ -0,0 +1,14 @@
+// Veal_Julie  July 24, 2013  Celsius to Fahrenheit converter
+
+//
+
+
+var degFaren = prompt ("Enter the degrees in Fahrenheit");
+
+var degCelsius = 5/9 * (degFaren-32);//I figured out what the calculation was in order to get Celsius
+
+
+
+	console.log("The temperature is " + degCelsius + " degrees Celsius");
+
+
