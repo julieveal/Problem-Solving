c756e8f6c7e1b4995fb7509958ab010ad9c04207
julieveal <julieclucas@att.net>
2013-07-25 00:36:16 -0400

    This took awhile to figure out. I am making things more difficult than they need to be!

---
+++ Conditionals_Worksheet/Grade Letter/js/script.js
@@ -0,0 +1,19 @@
+// Veal_Julie  July 24, 2013  Grades
+
+//grades are greater than 0 and less than 100
+//what is the grade
+
+var percent = 95;
+var grade = "A";
+
+
+if(percent > 93){
+
+
+	console.log("You have a " + percent + " %, which means you earned a " + grade + " in the class!");
+
+}else{
+	//if it's less than 93
+	console.log("You have a B");
+
+}	
