d6174ca2200a4a2af380fdc68d13802d6dc60030
julieveal <julieclucas@att.net>
2013-07-24 20:11:19 -0400

    starting

---
+++ Conditionals_Worksheet/js/script.js
@@ -0,0 +1,13 @@
+// Veal_Julie  Conditional logic - Ternary Operators
+
+var gpa = 48;
+
+//if the gpa is over min 2.0 score, the student can graduate
+/*if( gpa > 2.0){
+		console.log("you can graduate");
+
+}else{
+		console.log("gpa is too low!);
+}*/
+
+(gpa > 2.0)
