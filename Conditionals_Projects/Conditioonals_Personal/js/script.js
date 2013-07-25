d370f6ebc6edf71df0aaff07be7ecd140e7551b3
julieveal <julieclucas@att.net>
2013-07-25 19:04:07 -0400

    deciding on assignments

---
+++ Conditionals_Projects/Conditioonals_Personal/js/script.js
@@ -0,0 +1,20 @@
+// Veal_Julie  July 24, 2013  Tires
+
+//psi is ok between 30 and 35
+//do they pass spec
+
+
+var frontLeft = 34;
+var frontRight = 32;
+
+
+//if the tire pressure in between 30 and 35
+if(frontLeft < 36 && frontRight > 29){
+
+	console.log("Tires pass spec!");
+
+}else{
+
+	console.log("You should tires checked!");
+
+}	
