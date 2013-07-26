9735514143bf22fac48fc8e39235222015060283
julieveal <julieclucas@att.net>
2013-07-25 20:30:11 -0400

    console.log("Get a new photo!");   //the right size }else{							     		//otherwise

--- Conditionals_Projects/Conditionals_Industry/js/script.js
+++ Conditionals_Projects/Conditionals_Industry/js/script.js
@@ -10,7 +10,7 @@ var heightP = prompt("What is the height of your photograph in pixels?", 1000);	
 
 if(heightP && heightI){   //they are not the same number
 							
-	console.log("Get a new photo!");  //the right size
+	console.log("Get a new photo!");   //the right size
 }else{							     		//otherwise
 										
 	console.log("You should use a different tire!");  //not the right size
