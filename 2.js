8824644fb995496971e7daa90070bbd2371d3eec
julieveal <julieclucas@att.net>
2013-07-25 21:51:19 -0400

    .

---
+++ 2.js
@@ -0,0 +1,19 @@
+Cat, Parrot and Bag of Seed:
+Problem is we don’t know where the cabbage and the goat are. 
+We need to find out where the goat and cabbage are in order to advise a solution for getting the parrot, cat and seed to the other side of the riverbank.
+Cat can eat the goat and the goat will eat the cabbage but we don’t know if we can make multiple trips or not.
+He could make multiple trips but need to know which one should go first.
+We could take the goat first and then nothing would be eaten and go back and make 2 more trips.
+Socks in the Dark:
+The problem is I  can’t see in the dark.
+I need to select one matching pair at least.
+There are 3 different colors and 10 different pairs of socks. You would have to start out selecting 2 socks.
+That increases your chance of getting a match the next time. The quickest way to get there is to select 2 of the color with the lowest number of pairs. 
+
+Predicting Fingers:
+She doesn’t have enough fingers on one hand to count without repeating a finger. 
+We need to figure out what finger she will land on when she keeps counting in this certain order. Without counting all the way to 1000 with our fingers we need to do the math.  Which we could do to check our answer. If we don’t have a lot of time we need to do the math. 
+If she counts to 10 she will land on her pointer finger:
+10/5=2 which is the 2nd or pointer finger.
+If she counts to 100 she will land on her ring finger: Counting by 5s she would land on the 4th finger or the ring finger.
+If she counts to 1000 she will land on her pointer finger: Counting by 100s and land on the 2nd or pointer finger.
---
+++ script.js
@@ -0,0 +1,2 @@
+Cat, Parrot and Bag of Seed:
+Problem is we don’t know where the cabbage and the goat are. 
