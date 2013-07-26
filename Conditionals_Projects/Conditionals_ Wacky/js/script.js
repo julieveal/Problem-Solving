d466f9e319d920cd38b292769a544dd6d4aa2af6
julieveal <julieclucas@att.net>
2013-07-25 21:12:53 -0400

    size = ;

---
+++ Conditionals_Projects/Conditionals_ Wacky/js/script.js
@@ -0,0 +1,19 @@
+ // Veal_Julie  July 25, 2013  Wacky
+
+//What is the age of my tree?
+//rings = 1000
+
+var rings = prompt("What is the age of my tree?", 1000);		// years
+	age = prompt("What is the height of your photograph in inches?");		// inches wide
+	width = ;		//divide hP by hI
+	
+
+if(heightP && heightI){   //they are both true
+							
+	console.log("Use the photo!");   //the right size
+}else{							     		//otherwise
+										
+	console.log("Get a new photo!");   //not the right size
+													
+}												//close
+
