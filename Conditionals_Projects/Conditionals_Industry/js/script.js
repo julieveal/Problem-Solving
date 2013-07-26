684c34022ad990a137587d890fd362d5c9368d4f
julieveal <julieclucas@att.net>
2013-07-25 20:29:48 -0400

    if(heightP && heightI){   //they are not the same number

--- Conditionals_Projects/Conditionals_Industry/js/script.js
+++ Conditionals_Projects/Conditionals_Industry/js/script.js
@@ -5,15 +5,12 @@
 
 var heightP = prompt("What is the height of your photograph in pixels?", 1000);		// pixels wide
 	heightI = prompt("What is the height of your photograph in inches?");		// inches wide
-	res = heightP / heightI;		//multiply hP by hI
+	resolution = heightP / heightI;		//divide hP by hI
 
 
-var	res = prompt("What size is the photograph's resolution?");				//how tall is the tire?
-	console.log(res); 	 //calculate
-
-/*if(tireC == bikeHeight){   //they are the same number
+if(heightP && heightI){   //they are not the same number
 							
-	console.log("Put the tire on the bike!");  //the right size
+	console.log("Get a new photo!");  //the right size
 }else{							     		//otherwise
 										
 	console.log("You should use a different tire!");  //not the right size
