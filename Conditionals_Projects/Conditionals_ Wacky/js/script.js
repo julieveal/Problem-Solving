86c672b4ebe4fe4b091211411a1028a08618624b
julieveal <julieclucas@att.net>
2013-07-25 21:45:47 -0400

    if (diameter < radius){ 	console.log("start over");

--- Conditionals_Projects/Conditionals_ Wacky/js/script.js
+++ Conditionals_Projects/Conditionals_ Wacky/js/script.js
@@ -7,14 +7,16 @@ var circumference = prompt("What is the circumference of my tree?", 50);		// inc
 	diameter = circumference / 3.14;
 	radius = diameter / 2;	
 	age = radius / .125;
-
-
-//if(heightP && heightI){   //they are both true
 							
-	console.log("The age of the tree is " + age + " years.");   //the right size
-//}else{							     		//otherwise
-										
-//	console.log("Get a new photo!");   //not the right size
-													
-												//close
+if (diameter < radius){
+	console.log("start over");
+}
+else{
+	console.log("you are done");
+}
 
+
+
+
+
+	//console.log("The age of the tree is " + age + " years.");   //the right size
