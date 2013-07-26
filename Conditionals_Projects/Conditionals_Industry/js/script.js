ceb930155a6ade31862300de487d0549ace0558b
julieveal <julieclucas@att.net>
2013-07-25 20:38:51 -0400

    }else if{							     		//otherwise
    	console.log("Get a new photo!");   //not the right size
    
    }else{
    	heightP != heightI != resolution;
     	console.log("Recalculate!");
    
    }												//close

--- Conditionals_Projects/Conditionals_Industry/js/script.js
+++ Conditionals_Projects/Conditionals_Industry/js/script.js
@@ -11,9 +11,14 @@ var heightP = prompt("What is the height of your photograph in pixels?", 1000);	
 if(heightP && heightI){   //they are not the same number
 							
 	console.log("Use the photo!");   //the right size
-}else{							     		//otherwise
+}else if{							     		//otherwise
 										
 	console.log("Get a new photo!");   //not the right size
 													
+}else{
+	heightP != heightI != resolution;
+ 	console.log("Recalculate!");
+
+
 }												//close
 
