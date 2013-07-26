8df35f10eefc7cd122e88bb71690303c73219cc6
julieveal <julieclucas@att.net>
2013-07-25 21:49:12 -0400

    // Veal_Julie  July 25, 2013  Wacky
    //What is the age of my tree?
     //circumference = 36 inches
    
    var circumference = prompt("What is the circumference of my tree?", 50);		// inches around measured with a string
    	diameter = circumference / 3.14; //divide circum by pi which is 3.14
    	radius = diameter / 2;	//divide diamter by 2
    	age = radius / .125; // divide radius by .125 which is the approximate width of a ring
    
    if (diameter < radius){		//if diameter is less than the radius something is wrong and you should remeasure
    	console.log("start over");		//calculate
    }
    else {								//otherwise
    	console.log ("you are done");		//you are done
    }							//fini
    
    	//console.log("The age of the tree is " + age + " years.");   //the right size

--- Conditionals_Projects/Conditionals_ Wacky/js/script.js
+++ Conditionals_Projects/Conditionals_ Wacky/js/script.js
@@ -4,16 +4,16 @@
  //circumference = 36 inches
 
 var circumference = prompt("What is the circumference of my tree?", 50);		// inches around measured with a string
-	diameter = circumference / 3.14;
-	radius = diameter / 2;	
-	age = radius / .125;
+	diameter = circumference / 3.14; //divide circum by pi which is 3.14
+	radius = diameter / 2;	//divide diamter by 2
+	age = radius / .125; // divide radius by .125 which is the approximate width of a ring
 							
-if (diameter < radius){
-	console.log("start over");
+if (diameter < radius){		//if diameter is less than the radius something is wrong and you should remeasure
+	console.log("start over");		//calculate
 }
-else {
-	console.log ("you are done");
-}
+else {								//otherwise
+	console.log ("you are done");		//you are done
+}							//fini
 
 
 
