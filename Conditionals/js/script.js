57ac4956dd0283a119ed6b150436f74454d26e33
julieveal <julieclucas@att.net>
2013-08-02 22:15:30 -0400

    added // to try and get this to be pushed up

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -4,7 +4,7 @@ var gpa = 48;
 
 //if the gpa is over min 2.0 score, the student can graduate
 id( gpa > 2.0){
-		console.log("you can graduate");
+		console.log("you can graduate"); //
 
 }else{
 		console.log("gpa is too low!);
